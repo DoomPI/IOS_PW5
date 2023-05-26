d674505bf38641f59df38fa6e92ead8cc58fa3bb
DoomPI <rslomtev@edu.hse.ru>
2023-05-26 21:53:15 +0300

    Init commit

--- rslomtevPW5/Screens/NewsList/NewsListWorker.swift
+++ rslomtevPW5/Screens/NewsList/NewsListWorker.swift
@@ -13,7 +13,7 @@ class NewsListWorker: NewsListWorkerLogic {
     private let session = URLSession.shared
     
     func getNews(_ request: Model.GetNews.Request, completion: @escaping ((Model.NewsNetwork) -> ())) {
-        guard let url = URL(string: "https://newsapi.org/v2/everything?q=apple&from=2022-10-23&to=2022-10-23&sortBy=popularity&apiKey=ba80e2dd17a34e86a360b8faf4e5c7db") else {
+        guard let url = URL(string: "https://newsapi.org/v2/everything?q=apple&sortBy=popularity&apiKey=ba80e2dd17a34e86a360b8faf4e5c7db") else {
             return
         }
         let task = session.dataTask(with: url) { [weak self] data, response, error in
